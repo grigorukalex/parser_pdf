a6aa1e39d5f36796627573689ac3bd0dadd079fd
grigorukalex <grigorukalex@gmail.com>
2023-11-24 23:00:59 +0700

    -

--- examples/parser_pdf/example.py
+++ examples/parser_pdf/example.py
@@ -1,6 +1,6 @@
 from parser_pdf.bboxes import parse
 
-pdf_path = 'db/p1.pdf'
+pdf_path = 'db/testPDF.pdf'
 parse(pdf_path,
       skip_num_tables=4,    # Кол-во таблиц, которые нужно пропустить на первой странице
       padding=(1.0, 0.95)   # Отступ сверху и сниху от границ таблицы в % от ширины и высоты страницы
